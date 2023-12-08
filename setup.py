8a58b5d8d5665c3eeab624fb6c740d8c9ead47d0
Alex Reardon <arear001@ucr.edu>
2023-12-08 10:56:21 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='siqc',
-    version='0.0.4',
+    version='0.0.5',
     author='Reardon, Fonville, Avants',
     author_email='areardon@invicro.com',
     description='siqc',
